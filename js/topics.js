00a6897c67340510bbb7e828a8c2d7ad89e0611b
eshabnam-notcoder <eshabnam2025@gmail.com>
2026-07-02 16:12:52 +0530

    Modified topics.js

--- js/topics.js
+++ js/topics.js
@@ -1,8 +1,20 @@
 const topics = {
     "Basic Electrical": [
         {
+            title: "Fundamentals of Electricity",
+            file: "fundamentals-of-electricity"
+        },
+        {
+            title: "Circuit Elements",
+            file: "circuit-elements"
+        },
+        {
             title: "Ohm's Law",
             file: "ohms-law"
+        },
+        {
+            title: "Series and Parallel Circuits",
+            file: "series-and-parallel-circuits"
         },
         {
             title: "Kirchhoff's Current Law",
@@ -13,24 +25,125 @@ const topics = {
             file: "kvl"
         },
         {
-            title: "Series Circuit",
-            file: "series-circuit"
+            title: "Thevenin's Theorem",
+            file: "thevenins-theorem"
         },
         {
-            title: "Parallel Circuit",
-            file: "parallel-circuit"
+            title: "Norton's Theorem",
+            file: "nortons-theorem"
+        },
+         {
+            title: "Superposition Theorem",
+            file: "superposition-theorem"
+        },
+         {
+            title: "Maximum Power Transfer Theorem",
+            file: "max-power-transfer-theorem"
+        },
+         {
+            title: "DC Circuits",
+            file: "dc-circuits"
+        },
+         {
+            title: "AC Fundamentals",
+            file: "ac-fundamentals"
+        },
+         {
+            title: "AC Circuit Analysis",
+            file: "ac-circuit-analysis"
+        },
+         {
+            title: "Three phase circuits",
+            file: "three-phase-pcircuits"
+        },
+         {
+            title: "Magnetic Circuits",
+            file: "magnetic-circuits"
+        },
+         {
+            title: "Transformers",
+            file: "transformers"
+        },
+         {
+            title: "Electrical Machines",
+            file: "electrical-machines"
+        },
+         {
+            title: "Measuring Instruments",
+            file: "measuring-instruments"
+        },
+         {
+            title: "Electrical Wiring and Protection",
+            file: "electrical-wiring-and-protection"
+        },
+         {
+            title: "Power Generation, Transmission and Distribution",
+            file: "power-gen-transmission-and-dist"
         }
     ],
 
     "Power Systems": [
         {
-            title: "Generation",
-            file: "generation"
+            title: "Introduction to Power Systems",
+            file: "introduction-to-power-systems"
         },
         {
-            title: "Transmission",
-            file: "transmission"
+            title: "Thermal Power Plant",
+            file: "thermal-power-plant"
+        },
+        {
+            title: "Hydroelectric Power Plant",
+            file: "hydroelectric-power-plant"
+        },
+        {
+            title: "Nuclear Power Plant",
+            file: "nuclear-power-plant"
+        },
+        {
+            title: "Gas Turbine Power Plant",
+            file: "gas-turbine-power-plant"
+        },
+        {
+            title: "Diesel Power Plant",
+            file: "diesel-power-plant"
+        },
+        {
+            title: "Solar Power Plant",
+            file: "solar-power-plant"
+        },
+        {
+            title: "Wind Power Plant",
+            file: "wind-power-plant"
+        },
+        {
+            title: "Biomass Power Plant",
+            file: "biomass-power-plant"
+        },
+        {
+            title: "Combined Cycle Power Plant",
+            file: "combined-cycle-power-plant"
+        },
+        {
+            title: "Economic Dispatch Introduction",
+            file: "economic-dispatch"
+        },
+        {
+            title: "Transmission - Part 1",
+            file: "transmission-part1"
+        },
+         {
+            title: "Transmission - Part 2",
+            file: "transmission-part2"
+        },
+         {
+            title: "Transmission - Part 3",
+            file: "transmission-part3"
+        },
+         {
+            title: "Transmission - Part 4",
+            file: "transmission-part4"
         }
+
     ],
 
     "Electrical Machines": [
